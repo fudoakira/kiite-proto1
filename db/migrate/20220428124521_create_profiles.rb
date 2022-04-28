005bb1ec9d6a0abafa7ecc5d1ac846d131d86473
fudoakira <fumi.032010@gmail.com>
2022-04-28 22:18:02 +0900

    profileテーブル作成

--- db/migrate/20220428124521_create_profiles.rb
+++ db/migrate/20220428124521_create_profiles.rb
@@ -1,7 +1,14 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-
+      t.string :nickname, null: false
+      t.string :image
+      t.string :comment
+      t.text   :content
+      t.string :twitter
+      t.string :instagram
+      t.float  :rate
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
