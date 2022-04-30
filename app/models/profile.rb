5a158f9e4328c1e1ee98dfb8d798e53b132bd3cd
fudoakira <fumi.032010@gmail.com>
2022-04-30 22:25:12 +0900

    タグ付け機能の実装

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,26 @@ class Profile < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   belongs_to :user
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :rooms, through: :room_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
+  has_many :profile_tags, dependent: :destroy
+  has_many :tags, through: :profile_tags
 
   validates :nickname, presence: true
+
+  def save_tag(sent_tags)
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+
+    old_tags.each do |old|
+      self.tags.delete Tag.find_by(tag_name: old)
+    end
+
+    new_tags.each do |new|
+      new_profile_tag = Tag.find_or_create_by(tag_name: new)
+      self.tags << new_profile_tag
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: "profiles#index"
   resources :profiles
   resources :users, only: [:edit, :update]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :show]
+  resources :tags do
+    get "profiles", to: "profiles#search"
+  end
 end
